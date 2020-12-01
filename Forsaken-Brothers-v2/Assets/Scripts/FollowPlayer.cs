fed2f9c81fd4698fb2b3990a3f8eca891f39eb87
Dhanish Mouhamad <d.mouhamad@se18.qmul.ac.uk>
2020-12-01 20:58:44 +0000

    Mob created
    mob moves towards the players but dissappers

---
+++ Forsaken-Brothers-v2/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public float speed = 3;
+    private Transform target;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, target.transform.position) < 4)
+            transform.position = Vector2.MoveTowards(transform.position,   target.position, speed * Time.deltaTime);
+
+    }
+}
