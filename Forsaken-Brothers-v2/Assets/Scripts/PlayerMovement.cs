c4427d43bc831b3cf0b032d6454f8f740c3f4865
1min2midnight <mendezdemian@gmail.com>
2020-12-01 19:12:39 +0000

    player movement and animation

---
+++ Forsaken-Brothers-v2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public Animator animator;
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Input
+
+       movement.x = Input.GetAxisRaw("Horizontal");
+       movement.y = Input.GetAxisRaw("Vertical");
+       animator.SetFloat("Horizontal", movement.x);
+       animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+    private void FixedUpdate()
+    {
+        // player movement
+        rb.MovePosition(rb.position + movement *moveSpeed *Time.fixedDeltaTime);
+
+    }
+}
