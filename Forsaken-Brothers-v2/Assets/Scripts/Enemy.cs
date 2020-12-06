ae2af528c8048072ae643b0801751608f1614a13
Dhanish Mouhamad <d.mouhamad@se18.qmul.ac.uk>
2020-12-06 18:22:14 +0000

    Score working
    no conflicts

--- Forsaken-Brothers-v2/Assets/Scripts/Enemy.cs
+++ Forsaken-Brothers-v2/Assets/Scripts/Enemy.cs
@@ -24,6 +24,7 @@ public class Enemy : MonoBehaviour
 
     void Die()
     {
+        ScoreManager.currentScore += 20;
         SoundManagerScript.PlaySound("enemydead");
         GameObject.Destroy(gameObject);
         Debug.Log("enemy died");
