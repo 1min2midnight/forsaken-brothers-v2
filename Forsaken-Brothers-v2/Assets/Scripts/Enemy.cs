419199423829c7ea20ba782846e77a75d6f1edeb
Dhanish Mouhamad <d.mouhamad@se18.qmul.ac.uk>
2020-12-06 18:09:54 +0000

    score working
    enemy conflicted??

---
+++ Forsaken-Brothers-v2/Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int maxHealth = 100;
+    int currentHealth;
+    
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+        
+    }
+
+    void Die()
+    {
+        Scorescript.currentScore += 20;
+        SoundManagerScript.PlaySound("enemydead");
+        GameObject.Destroy(gameObject);
+        Debug.Log("enemy died");
+
+    }
+
+}
