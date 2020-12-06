c29827506f477da78af9332b4b428f4c59d69136
Inveniumm <48212653+Inveniumm@users.noreply.github.com>
2020-12-06 14:55:23 +0000

    Death scene scripted

--- Forsaken-Brothers-v2/Assets/Scripts/PlayerHealth.cs
+++ Forsaken-Brothers-v2/Assets/Scripts/PlayerHealth.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -56,6 +57,7 @@ public class PlayerHealth : MonoBehaviour
         if (health <= 0)
         {
             GameObject.Destroy(gameObject);
+            SceneManager.LoadScene("GameOver");
         }
     }
 
