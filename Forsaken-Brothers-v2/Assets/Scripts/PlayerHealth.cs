1b2dbd5db18e1acc0fac21e9f21c70bd57bef46d
Dhanish Mouhamad <d.mouhamad@se18.qmul.ac.uk>
2020-12-07 00:22:28 +0000

    healthbar added
    works !!

--- Forsaken-Brothers-v2/Assets/Scripts/PlayerHealth.cs
+++ Forsaken-Brothers-v2/Assets/Scripts/PlayerHealth.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField]
+    public int maxHealth = 100;
     public int health;
     public int durationYellow = 5;
 
