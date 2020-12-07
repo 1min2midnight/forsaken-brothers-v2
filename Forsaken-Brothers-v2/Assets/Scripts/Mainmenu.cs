56f8eec48e84bc4de5600888e93fcc4b42696c60
Inveniumm <48212653+Inveniumm@users.noreply.github.com>
2020-12-07 10:38:09 +0000

    Fixed save method

--- Forsaken-Brothers-v2/Assets/Scripts/Mainmenu.cs
+++ Forsaken-Brothers-v2/Assets/Scripts/Mainmenu.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class Mainmenu : MonoBehaviour
 {
+    private int sceneToContinue;
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -16,6 +17,15 @@ public class Mainmenu : MonoBehaviour
     public void GoToMainmenu()
     {
         SceneManager.LoadScene("Mainmenu");
+    }
+    public void LoadTheSavedScene()
+    {
+        sceneToContinue = PlayerPrefs.GetInt("SavedScene");
+        if (sceneToContinue != 0)
+           SceneManager.LoadScene(sceneToContinue);
+        else
+           return;   
+        
     }
     public void QuitGame()
     {
